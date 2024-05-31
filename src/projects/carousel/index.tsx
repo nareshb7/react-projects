5ab1d6d5a3d08781fd5841826328685ebabcac72
nareshb7 <nareshbjava7@gmail.com>
2024-05-31 15:25:33 +0530

    11. carousel implemented

--- src/projects/carousel/index.tsx
+++ src/projects/carousel/index.tsx
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import "./styles.scss";
+import { Button } from "common/Components";
+import { slides } from "./slides";
 
-const Carousel =()=> {
-    return <div>CAROUSEL</div>
-}
+const Carousel = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideInterval = useRef<NodeJS.Timeout>();
+  const handlePrev = () => {
+    setCurrentSlide((currentSlide - 1 + slides.length) % slides.length);
+  };
+  const handleNext = () => {
+    setCurrentSlide((currentSlide + 1) % slides.length);
+  };
+  const goToSlide = (slide: number) => {
+    setCurrentSlide(slide);
+  };
+  const startAutoPlay = () => {
+    stopAutoPlay();
+    slideInterval.current = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % slides.length);
+    }, 1000);
+  };
+  const stopAutoPlay = () => {
+    if (slideInterval.current) {
+      clearInterval(slideInterval.current);
+    }
+  };
+  useEffect(() => {
+    startAutoPlay();
+    return () => stopAutoPlay();
+  }, [currentSlide]);
+  console.log("CURRENT:::", currentSlide);
+  return (
+    <div
+      className="carousel-main"
+      onMouseEnter={stopAutoPlay}
+      onMouseLeave={startAutoPlay}
+    >
+      <div>CAROUSEL</div>
+      <div className="carousel-slides">
+        {slides.map((slide, i) => (
+          <div
+            key={i}
+            className={`carousel-slide ${i === currentSlide ? "active" : ""}`}
+            style={{
+              backgroundImage: `url(${slide})`,
+              color: "#fff",
+              transform: `translateX(-${currentSlide * 100}%)`,
+            }}
+          >
+            Image {i + 1}
+          </div>
+        ))}
+      </div>
+      <Button
+        className="carousel-control prev"
+        title="&#10094;"
+        onClick={handlePrev}
+      />
+      <Button
+        className="carousel-control next"
+        title="&#10095;"
+        onClick={handleNext}
+      />
+      <div className="carousel-indicators">
+        {slides.map((_, i) => (
+          <span
+            key={i}
+            className={`carousel-indicator ${
+              i === currentSlide ? "active" : ""
+            }`}
+            onClick={() => goToSlide(i)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Carousel
+export default Carousel;
---
+++ src/projects/carousel/slides.ts
@@ -0,0 +1,17 @@
+const url =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSQ-nNusVIbConk7KFewtvY3kA49dBpNOL_Q&s";
+export const urls = [
+  "https://c4.wallpaperflare.com/wallpaper/131/774/322/background-pictures-nature-hd-images-1920x1200-wallpaper-preview.jpg",
+//   "https://png.pngtree.com/thumb_back/fh260/background/20211031/pngtree-abstract-bg-image_914283.png",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSQ-nNusVIbConk7KFewtvY3kA49dBpNOL_Q&s",
+  "https://www.shutterstock.com/image-photo/beautiful-blurred-background-image-spring-260nw-2213392711.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1thtGw19xJXJ7AsbMpi6CVXonbSyBRUZYFg&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScgtgYiF_2WUWXY_JMPjwB6-VLo14vruQbAw&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRG0uMSr7M7igLqDoyBbnzvM608YqqE2vUuyw&s",
+  "https://static.vecteezy.com/system/resources/thumbnails/026/365/937/small/beautiful-blurred-green-nature-background-ai-generated-photo.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwrwE4uObcZl69GFijAWH8ZIVHFPdjSjKY0w&s",
+  "https://static.vecteezy.com/system/resources/thumbnails/030/353/613/small/natural-background-ai-generated-photo.jpg",
+  "https://wallpaperset.com/w/full/c/9/0/522708.jpg",
+  "https://i.pinimg.com/736x/f1/5d/ea/f15deaa797aaa5901d514fde36a51ea9.jpg",
+];
+export const slides = urls
