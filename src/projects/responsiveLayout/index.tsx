9a2780c3d8aa45da801fd43a4d48e979de55a079
nareshb7 <nareshbjava7@gmail.com>
2024-06-07 07:53:17 +0530

    responselayout index file

--- src/projects/responsiveLayout/index.tsx
+++ src/projects/responsiveLayout/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import './styles.scss'
+import React from "react";
+import "./styles.scss";
+import Conatiner from "./components/Conatiner";
 
 const ResponsiveLayout = () => {
   return (
-    <div className='responsive-layout-wrapper'>Responsive Layout</div>
-  )
-}
+    <div className="responsive-layout-wrapper">
+      <div className="header-title">Responsive Layout</div>
+      <Conatiner />
+    </div>
+  );
+};
 
-export default ResponsiveLayout
+export default ResponsiveLayout;
