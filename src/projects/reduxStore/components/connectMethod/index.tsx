12cd7a215e1a52593cddcd31c6279c9e2bc1138f
nareshb7 <nareshbjava7@gmail.com>
2024-06-07 15:48:01 +0530

    13. connect error fix

--- src/projects/reduxStore/components/connectMethod/index.tsx
+++ src/projects/reduxStore/components/connectMethod/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Connect from "./Connect";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const ConnectMethod = () => {
   return (
+    <Provider store={store}>
     <div className="connect-method">
       <div className="font-bold text-center">ConnectMethod</div>
       <Connect />
     </div>
+    </Provider>
   );
 };
 
