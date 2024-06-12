3673d07b094544b3985cebd44abc5d1ad5775af7
nareshb7 <nareshbjava7@gmail.com>
2024-06-12 08:04:35 +0530

    scroll to top function added

--- src/utils/util.tsx
+++ src/utils/util.tsx
@@ -1,3 +1,12 @@
 export const isEmptyObject = (object: Object) => {
-    return Object.keys(object).length == 0;
-  };
+  return Object.keys(object).length == 0;
+};
+
+export const scrollToTop = (ref: React.RefObject<HTMLElement>) => {
+  if (ref.current) {
+    ref?.current.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+};
